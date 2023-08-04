d7c43573e6d27f74bd77aa6722fbba1970f5d27d
Dmitry Soboliev <19leva89@gmail.com>
2023-08-04 16:46:37 +0300

    create template-dashboard

--- src/route/index.js
+++ src/route/index.js
@@ -1091,7 +1091,15 @@ router.get('/template-slack', function (req, res) {
   res.render('template-slack', {
     layout: null,
   })
-  //                  ↑↑ сюди вводимо JSON дані
+})
+
+router.get('/dashboard', function (req, res) {
+  // res.render генерує нам HTML сторінку
+
+  //            ↙ cюди вводимо назву файлу з сontainer
+  res.render('dashboard', {
+    layout: null,
+  })
 })
 
 // ================================================================
